77bab4fba3ab6276335dd21b9c10e182b0852a9e
chanhokwong <chanhokwong718@gmail.com>
2025-06-30 22:44:33 +0800

    Delete FlowerClassification directory

--- FlowerClassification/ReadLabels.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("flower_classifier/label_map.json", "r") as f:
-    flower_labels = json.load(f)
-    print(flower_labels)
-
-# 格式如：{0: 'pink primrose', 1: 'hard-leaved pocket orchid', ...}
